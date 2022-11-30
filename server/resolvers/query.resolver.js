c1748958ec0d47e4b44a800b8225c9355a2f8eff
BMidhun <midhunbabygeorge@gmail.com>
2022-11-30 20:04:59 +0530

    add durationInSeconds in query resolver

--- server/resolvers/query.resolver.js
+++ server/resolvers/query.resolver.js
@@ -1,30 +1,44 @@
 exports.queryResolver = {
     Query: {
         tracksForHome: (parent, args, context, info) => {
-            const {dataSources} = context;
+            const { dataSources } = context;
             return dataSources.trackAPI.getTracksForHome();
         },
 
-        track : (_, args, context) => {
-            const {dataSources} = context;
-            const {id} = args;
+        track: (_, args, context) => {
+            const { dataSources } = context;
+            const { id } = args;
             return dataSources.trackAPI.getTrack(id);
         }
     },
 
     Track: {
-        author: (parent, args,context,info) => {
-            const {authorId} = parent;
-            const {dataSources} = context;
+        author: (parent, args, context, info) => {
+            const { authorId } = parent;
+            const { dataSources } = context;
 
             return dataSources.trackAPI.getAuthor(authorId);
         },
 
         modules: (parent, _, context) => {
-            const {dataSources} = context;
-            const {id} = parent;
-            
+            const { dataSources } = context;
+            const { id } = parent;
+
             return dataSources.trackAPI.getModules(id);
+        },
+
+        durationInSeconds: (parent) => {
+            const { length } = parent;
+            return length;
         }
-    }   
+
+    },
+
+    Module: {
+        durationInSeconds: (parent) => {
+            const { length } = parent;
+            return length;
+        }
+
+    }
 }
